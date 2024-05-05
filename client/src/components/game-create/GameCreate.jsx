a7cb57c36ccf724cb8b686208653be977a78d64d
Velislav99 <velislavchoranov@gmail.com>
2024-05-05 15:54:25 +0200

    Create Game service

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,7 +1,15 @@
 export default function GameCreate() {
+
+    const createGameSubmiteHandler = (e) =>{
+        e.preventDefault();
+
+        const gameData = Object.fromEntries(new FormData(e.currentTarget))
+        console.log(gameData)
+    }
+
     return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={createGameSubmiteHandler}>
                 <div className="container">
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
